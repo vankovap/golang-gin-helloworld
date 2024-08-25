2f25aa1c6a5ac0b1fd6154fae94d1f75e09028f4
vankovap <petravankova.pv@gmail.com>
2024-08-25 18:38:54 +0200

    Update main.go

--- main.go
+++ main.go
@@ -10,14 +10,13 @@ import (
 )
 
 func main() {
-        fmt.Println("Running http server")
-        slog.Info("info")
-        slog.Warn("warn")
-        slog.Debug("debug")
+        port := os.Getenv("APP_PORT")
+        fmt.Printf("Running http server on port :%s", port)
+        fmt.Println()
 
         router := gin.Default()
         router.GET("/hello-world", getHelloWorld)
-        log.Fatal(router.Run(os.Getenv("APP_PORT")))
+        log.Fatal(router.Run(":"+port))
 }
 
 func getHelloWorld(c *gin.Context) {
